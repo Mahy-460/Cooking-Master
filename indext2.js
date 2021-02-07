8785ac9990b761ae98430532bb3dc8cfc02bf056
mahy-460 <imtiushasan420@gmail.com>
2021-02-07 23:29:50 +0600

    alhamdullila

--- indext2.js
+++ indext2.js
@@ -14,9 +14,9 @@ function searchMeal(e) {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+        resultHeading.innerHTML = `<h2 class="h2">Search results for '${term}':</h2>`;
         if (data.meals === null) {
-          resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+          resultHeading.innerHTML = `<h2 class="h2">There are no search results. Try again!</h2>`;
         } else {
           mealsEl.innerHTML = data.meals
             .map(
@@ -24,7 +24,7 @@ function searchMeal(e) {
             <div class="meal">
             <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
             <div class="meal-info" data-mealID="${meal.idMeal}">
-            <button onclick="displayCountryDetail('${meal.name}')">Details</button>
+           
             <h3>${meal.strMeal}</h3></div>
             </div>`
             )
@@ -37,49 +37,50 @@ function searchMeal(e) {
   }
       
 }
+function getMealById(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const meal = data.meals[0];
+        addMealToDOM(meal);
+      });
+  }
+  function addMealToDOM(meal) {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        ingredients.push(
+          `${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`
+        );
+      } else {
+        break;
+      }
+    }
+    single_mealEl.innerHTML = `
+    <div class="single-meal">
+               <h1>Chorize & mozzarell</h1>
+               <h4>Ingredients</h4>
+          <ul>
+              ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+          </ul>
+      </div>
+    </div>`;
+  }
 submit.addEventListener("submit", searchMeal);
+mealsEl.addEventListener('click', (event) => {
+    const mealInfo = event.path.find((item) => {
+        if(item.classList){
+            return item.classList.contains('meal-info')
+        }
+        else{
+            return false;
+        }
+    })
+    if(mealInfo){
+      
+        const mealID = mealInfo.getAttribute('data-mealid')
+        getMealById(mealID);
+    }
+})
 
 
-
-// fetch('https://restcountries.eu/rest/v2/all')
-// .then(res => res.json())
-// .then(data => displayCountry(data))
-
-
-// const displayCountry = countries =>{
-//     const cDiv = document.getElementById('countris');
-//     countries.forEach(country=> {
-        
-//        const countryDiv = document.createElement('div');  
-//         countryDiv.className = 'country';
-//         const info = `
-//            <h3 class="country-name">${country.name}</h3>
-//            <p>${country.capital}</p>
-//            <button onclick="displayCountryDetail('${country.name}')">Details</button>
-           
-//         `;
-//         countryDiv.innerHTML = info;
-//         cDiv.appendChild(countryDiv);
-//     });
-
-
-// const displayCountryDetail = name=>{
-//     const url = `https://restcountries.eu/rest/v2/name/${name}`
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => renderCountryInfo(data[0]));
-// }
-
-// const renderCountryInfo = country =>{
-//     console.log(country)
-//     const countryDiv = document.getElementById('countryDetail');
-//     let ful = countryDiv.innerHTML =`
-//         <h1>Country-Name :  ${country.name}</h1>
-//         <h3>Capital :  ${country.capital}</h3>
-//         <p>Population :  ${country.population}</p>
-//         <p>Area :  ${country.area}<p>
-//         <p>Time : ${country.timezones}</p>
-//         <img src="  ${country.flag}">
-        
-//     `
-// }
